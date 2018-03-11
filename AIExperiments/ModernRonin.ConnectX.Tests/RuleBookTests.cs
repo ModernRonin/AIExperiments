91e8264ac4a8c3fc28729f60e6f0dac58fe391ad
max <7413294+ModernRonin@users.noreply.github.com>
2018-03-11 21:42:36 +0100

    first test for RuleBook

--- AIExperiments/ModernRonin.ConnectX.Tests/RuleBookTests.cs
+++ AIExperiments/ModernRonin.ConnectX.Tests/RuleBookTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace ModernRonin.ConnectX.Tests
@@ -9,7 +10,8 @@ namespace ModernRonin.ConnectX.Tests
     {
         void Set(Board board, string input)
         {
-            var rows = input.Split(new[] {' ', '\r', '\n', '\t'}, StringSplitOptions.RemoveEmptyEntries).Reverse().ToArray();
+            var rows = input.Split(new[] {' ', '\r', '\n', '\t'}, StringSplitOptions.RemoveEmptyEntries).Reverse()
+                            .ToArray();
 
             int map(char c)
             {
@@ -22,6 +24,7 @@ namespace ModernRonin.ConnectX.Tests
 
                 throw new IndexOutOfRangeException();
             }
+
             void set(int x, int y)
             {
                 board[x, y] = new Stone {Owner = map(rows[y][x])};
@@ -29,17 +32,25 @@ namespace ModernRonin.ConnectX.Tests
 
             7.By(6).Do(set);
         }
+        Game SetupGame(string board)
+        {
+            var result = new Game(GameConfiguration.Default);
+            Set(result.Board, board);
+            return result;
+        }
         [Test]
         public void ResultFor_Discovers_Victory()
         {
-            var board = @".......
-                          .......
-                          x...x.o
-                          x...o.o
-                          x...o.o
-                          x...o.x";
+            const string board = @".......
+                                   .......
+                                   x...x.o
+                                   x...o.o
+                                   x...o.o
+                                   x...o.x";
+            var game= SetupGame(board);
 
-            var game = new Game(GameConfiguration.Default);
+            var result = new RuleBook().ResultFor(game);
+            result.Should().Be(GameResult.Victory);
         }
     }
 }
