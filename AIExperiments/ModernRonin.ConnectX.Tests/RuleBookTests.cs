80ea531754db61d1a5201674cf730fb31c555cb8
max <7413294+ModernRonin@users.noreply.github.com>
2018-03-11 21:34:16 +0100

    making it easy to write board-state related tests

--- AIExperiments/ModernRonin.ConnectX.Tests/RuleBookTests.cs
+++ AIExperiments/ModernRonin.ConnectX.Tests/RuleBookTests.cs
@@ -10,7 +10,24 @@ namespace ModernRonin.ConnectX.Tests
         void Set(Board board, string input)
         {
             var rows = input.Split(new[] {' ', '\r', '\n', '\t'}, StringSplitOptions.RemoveEmptyEntries).Reverse().ToArray();
-            
+
+            int map(char c)
+            {
+                switch (c)
+                {
+                    case '.': return -1;
+                    case 'x': return 0;
+                    case 'o': return 1;
+                }
+
+                throw new IndexOutOfRangeException();
+            }
+            void set(int x, int y)
+            {
+                board[x, y] = new Stone {Owner = map(rows[y][x])};
+            }
+
+            7.By(6).Do(set);
         }
         [Test]
         public void ResultFor_Discovers_Victory()
