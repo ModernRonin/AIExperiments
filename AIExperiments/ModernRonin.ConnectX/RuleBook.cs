330af4512a81ec546d5085ede8b3c08d20b4f0ff
max <7413294+ModernRonin@users.noreply.github.com>
2018-03-11 22:27:32 +0100

    RuleBook.LegalMoves test pass

--- AIExperiments/ModernRonin.ConnectX/RuleBook.cs
+++ AIExperiments/ModernRonin.ConnectX/RuleBook.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace ModernRonin.ConnectX
 {
     public class RuleBook
     {
-        public IEnumerable<Move> LegalMoves(Game game) => null;
+        public IEnumerable<Move> LegalMoves(Game game)
+        {
+            var board = game.Board;
+
+            bool isColumnFree(int x) => Enumerable.Range(0, board.Height).Any(y => board[x, y].Owner == -1);
+
+            var validX = Enumerable.Range(0, board.Width).Where(isColumnFree);
+            return validX.Select(x => new Move {X = x}).ToArray();
+        }
         public GameResult ResultFor(Game game) => GameResult.Undecided;
     }
 }
