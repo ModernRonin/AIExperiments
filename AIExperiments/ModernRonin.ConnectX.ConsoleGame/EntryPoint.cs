de3365c1e4f9e87eec87fa25ad9c74bcbe39a11e
max <7413294+ModernRonin@users.noreply.github.com>
2018-03-12 14:02:22 +0100

    EntryPoint knows to create a treesearch opponent

--- AIExperiments/ModernRonin.ConnectX.ConsoleGame/EntryPoint.cs
+++ AIExperiments/ModernRonin.ConnectX.ConsoleGame/EntryPoint.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Fclp;
+using ModernRonin.ConnectX.TreeSearchBot;
 
 namespace ModernRonin.ConnectX.ConsoleGame
 {
@@ -7,33 +8,52 @@ namespace ModernRonin.ConnectX.ConsoleGame
     {
         static void Main(string[] args)
         {
-            var configuration = ParseArguments(args);
+            var (configuration, opponentType) = ParseArguments(args);
             if (null == configuration) return;
 
-            var runner = new ConsoleRunner(new Game(configuration),
-                new[] {new HumanConsolePlayer(), new HumanConsolePlayer()});
+            var opponent = GetOpponent(opponentType);
+            var runner = new ConsoleRunner(new Game(configuration), new[] {new HumanConsolePlayer(), opponent});
             runner.Run();
             Console.WriteLine("Press <Enter> to exit...");
             Console.ReadLine();
         }
-        static GameConfiguration ParseArguments(string[] args)
+        static IPlayer GetOpponent(OpponentType opponentType)
         {
-            var result= new GameConfiguration();
+            switch (opponentType)
+            {
+                case OpponentType.Human: return new HumanConsolePlayer();
+                case OpponentType.TreeSearch: return new TreeSearchBotPlayer(6);
+            }
+
+            return null;
+        }
+        static (GameConfiguration, OpponentType) ParseArguments(string[] args)
+        {
+            var resultConfig = new GameConfiguration();
+            var resultOpponent = OpponentType.Human;
+
             var parser = new FluentCommandLineParser();
             parser.Setup<int>('w', "width").SetDefault(GameConfiguration.Default.BoardWidth)
-                  .Callback(w => result.BoardWidth = w);
+                  .Callback(w => resultConfig.BoardWidth = w);
             parser.Setup<int>('h', "height").SetDefault(GameConfiguration.Default.BoardHeight)
-                  .Callback(h => result.BoardHeight = h);
-            
+                  .Callback(h => resultConfig.BoardHeight = h);
+            parser.Setup<OpponentType>('o', "opponent").Required().Callback(o => resultOpponent = o);
+
             var parsed = parser.Parse(args);
             if (parsed.HasErrors)
             {
                 parser.HelpOption.ShowHelp(parser.Options);
-                return null;
+                return (null, resultOpponent);
             }
 
-            result.InitialStonesPerPlayer = GameConfiguration.Default.InitialStonesPerPlayer;
-            return result;
+            resultConfig.InitialStonesPerPlayer = GameConfiguration.Default.InitialStonesPerPlayer;
+            return (resultConfig, resultOpponent);
+        }
+
+        enum OpponentType
+        {
+            Human,
+            TreeSearch
         }
     }
 }
