6c0ead0257542f5ad92a2a0afc796690ab3a6bf7
max <7413294+ModernRonin@users.noreply.github.com>
2018-03-12 12:08:01 +0100

    refactored

--- AIExperiments/ModernRonin.ConnectX.TreeSearchBot/NegaMax.cs
+++ AIExperiments/ModernRonin.ConnectX.TreeSearchBot/NegaMax.cs
@@ -5,15 +5,15 @@ namespace ModernRonin.ConnectX.TreeSearchBot
 {
     public static class TreeSearch
     {
-        public static (int, IEnumerable<TMove>) NegaMax<TMove>(IGameState<TMove> startState, int maxDepth, int factor= 1)
+        public static (int, IEnumerable<TMove>) NegaMax<TMove>(IGameState<TMove> startState, int maxDepth, int evaluationSign= 1)
         {
-            if (0 == maxDepth) return (factor*startState.Evaluation, new TMove[0]);
+            if (0 == maxDepth) return (evaluationSign*startState.Evaluation, new TMove[0]);
             var bestEval = int.MinValue;
             List<TMove> bestLine = null;
             foreach (var move in startState.LegalMoves)
             {
                 var newBoard = startState.Execute(move);
-                var (eval, line) = NegaMax(newBoard, maxDepth - 1, -factor);
+                var (eval, line) = NegaMax(newBoard, maxDepth - 1, -evaluationSign);
                 eval *= -1;
                 if (eval > bestEval)
                 {
