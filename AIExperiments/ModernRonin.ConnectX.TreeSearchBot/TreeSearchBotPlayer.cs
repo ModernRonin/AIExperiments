15876c50311761798efc28c1228fffb126a722c8
max <7413294+ModernRonin@users.noreply.github.com>
2018-03-12 13:51:23 +0100

    cleanup

--- AIExperiments/ModernRonin.ConnectX.TreeSearchBot/TreeSearchBotPlayer.cs
+++ AIExperiments/ModernRonin.ConnectX.TreeSearchBot/TreeSearchBotPlayer.cs
@@ -5,13 +5,10 @@ namespace ModernRonin.ConnectX.TreeSearchBot
     public class TreeSearchBotPlayer : IPlayer
     {
         readonly int mMaxDepth;
-        public TreeSearchBotPlayer(int maxDepth)
-        {
-            mMaxDepth = maxDepth;
-        }
+        public TreeSearchBotPlayer(int maxDepth) => mMaxDepth = maxDepth;
         public Move GetMove(IRuleBook rules, IGame game)
         {
-            var (eval, bestLine) = TreeSearch.NegaMax(new ConnectXGameState(rules, game), mMaxDepth);
+            var (_, bestLine) = TreeSearch.NegaMax(new ConnectXGameState(rules, game), mMaxDepth);
             return bestLine.First();
         }
     }
