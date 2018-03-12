e11b647c85c32c54ca574280bd206c68b8e18198
max <7413294+ModernRonin@users.noreply.github.com>
2018-03-12 11:45:06 +0100

    added alternative API for test gamestate

--- AIExperiments/ModernRonin.ConnectX.TreeSearchBot.Tests/TreeSearchTests.cs
+++ AIExperiments/ModernRonin.ConnectX.TreeSearchBot.Tests/TreeSearchTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace ModernRonin.ConnectX.TreeSearchBot.Tests
@@ -6,20 +8,28 @@ namespace ModernRonin.ConnectX.TreeSearchBot.Tests
     [TestFixture]
     public class TreeSearchTests
     {
-        class GameState : IGameState<char>
+        class GameState : IGameState<char>, IEnumerable<GameState>
         {
             readonly Dictionary<char, GameState> mMoves = new Dictionary<char, GameState>();
+            public string State { get; set; }
+            public IEnumerator<GameState> GetEnumerator() => mMoves.Values.GetEnumerator();
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
             public IEnumerable<char> LegalMoves => mMoves.Keys;
-            public int Evaluation { get; private set; }
+            public int Evaluation { get; set; }
             public IGameState<char> Execute(char move) => mMoves[move];
             public GameState AfterMove(char move)
             {
                 if (mMoves.ContainsKey(move)) return mMoves[move];
-                var result = new GameState();
+                var result = new GameState {State = State + move};
                 mMoves[move] = result;
                 return result;
             }
             public void EvaluatesAs(int evaluation) => Evaluation = evaluation;
+            public void Add(GameState child)
+            {
+                var move = child.State.Last();
+                mMoves[move] = child;
+            }
         }
 
         /* 0
@@ -34,6 +44,7 @@ namespace ModernRonin.ConnectX.TreeSearchBot.Tests
             startState.AfterMove('A').EvaluatesAs(10);
             startState.AfterMove('B').EvaluatesAs(9);
             startState.AfterMove('C').EvaluatesAs(13);
+
         }
     }
 }
