49cc70ba3ecee550314621fc1e8cbcc655d6d3ce
max <7413294+ModernRonin@users.noreply.github.com>
2018-03-16 13:09:20 +0100

    AlphaBetaNegaMax implementation sketch

--- AIExperiments/ModernRonin.ConnectX.TreeSearchBot/NegaMax.cs
+++ AIExperiments/ModernRonin.ConnectX.TreeSearchBot/NegaMax.cs
@@ -1,13 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ModernRonin.ConnectX.TreeSearchBot
 {
     public static class TreeSearch
     {
-        public static (int, IEnumerable<TMove>) NegaMax<TMove>(IGameState<TMove> startState, int maxDepth, int evaluationSign= 1)
+        static (int, TMove[]) StaticEvaluation<TMove>(IGameState<TMove> startState, int sign) =>
+            (sign * startState.Evaluation, new TMove[0]);
+        public static (int, IEnumerable<TMove>) NegaMax<TMove>(
+            IGameState<TMove> startState,
+            int maxDepth,
+            int evaluationSign = 1)
         {
-            (int, TMove[]) staticEvaluation() => (evaluationSign * startState.Evaluation, new TMove[0]);
+            (int, TMove[]) staticEvaluation() => StaticEvaluation(startState, evaluationSign);
 
             if (0 == maxDepth) return staticEvaluation();
             var bestEval = int.MinValue;
@@ -26,7 +32,40 @@ namespace ModernRonin.ConnectX.TreeSearchBot
             }
 
             if (bestLine == null) return staticEvaluation();
-            
+
+            return (bestEval, bestLine);
+        }
+        public static (int, IEnumerable<TMove>) AlphaBetaNegaMax<TMove>(
+            IGameState<TMove> startState,
+            int maxDepth,
+            Func<IEnumerable<TMove>, IOrderedEnumerable<TMove>> moveSorter,
+            int alpha = int.MinValue,
+            int beta = int.MaxValue,
+            int evaluationSign = 1)
+        {
+            (int, TMove[]) staticEvaluation() => StaticEvaluation(startState, evaluationSign);
+
+            if (0 == maxDepth) return staticEvaluation();
+            var bestEval = int.MinValue;
+            List<TMove> bestLine = null;
+            foreach (var move in moveSorter(startState.LegalMoves))
+            {
+                var newBoard = startState.Execute(move);
+                var (eval, line) = AlphaBetaNegaMax(newBoard, maxDepth - 1, moveSorter, -alpha, -beta - evaluationSign);
+                eval *= -1;
+                if (eval > bestEval)
+                {
+                    bestEval = eval;
+                    bestLine = line.ToList();
+                    bestLine.Insert(0, move);
+                }
+
+                alpha = Math.Max(alpha, bestEval);
+                if (alpha >= beta) break;
+            }
+
+            if (bestLine == null) return staticEvaluation();
+
             return (bestEval, bestLine);
         }
     }
